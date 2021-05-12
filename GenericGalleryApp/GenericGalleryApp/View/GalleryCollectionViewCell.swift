e4a2b151460dd526875e44c66e4e3e2b05ecd507
AlexZhadaev <pvt.vatnik@gmail.com>
2021-05-12 23:34:47 +0300

    layout added

---
+++ GenericGalleryApp/GenericGalleryApp/View/GalleryCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  GalleryCollectionViewCell.swift
+//  GenericGalleryApp
+//
+//  Created by Жадаев Алексей on 12.05.2021.
+//
+
+import UIKit
+
+class GalleryCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ GenericGalleryApp/GenericGalleryApp/ViewController/GalleryCollectionLayoutExtension.swift
@@ -0,0 +1,30 @@
+//
+//  GalleryCollectionLayoutExtension.swift
+//  GenericGalleryApp
+//
+//  Created by Жадаев Алексей on 12.05.2021.
+//
+
+import UIKit
+
+extension GalleryCollectionViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let spacingWidth = sectionInsets.left * (itemsPerRow + 1)
+        let availableWidth = collectionView.frame.width - spacingWidth
+        let widthPerItem = availableWidth / itemsPerRow
+        return CGSize(width: widthPerItem, height: widthPerItem)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return sectionInsets
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return sectionInsets.left
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return sectionInsets.left
+    }
+}
