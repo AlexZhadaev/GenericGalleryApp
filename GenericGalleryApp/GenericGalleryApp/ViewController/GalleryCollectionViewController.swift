01812c7de5b1145780f532a3dd0f458643b0ed6a
AlexZhadaev <pvt.vatnik@gmail.com>
2021-05-14 00:02:24 +0300

    added photo and cell configure

--- GenericGalleryApp/GenericGalleryApp/ViewController/GalleryCollectionViewController.swift
+++ GenericGalleryApp/GenericGalleryApp/ViewController/GalleryCollectionViewController.swift
@@ -8,13 +8,13 @@
 import UIKit
 
 class GalleryCollectionViewController: UICollectionViewController {
-
+    
+    let photos = ["IMG_1", "IMG_2", "IMG_3", "IMG_3", "IMG_4", "IMG_5", "IMG_6", "IMG_7", "IMG_8", "IMG_9", "IMG_10", "IMG_11", "IMG_12", "IMG_13", "IMG_14", "IMG_15", "IMG_16", "IMG_17", "IMG_18", "IMG_19", "IMG_20", "IMG_21", "IMG_22"]
     let itemsPerRow: CGFloat = 3
     let sectionInsets = UIEdgeInsets(top: 20, left: 20, bottom: 20, right: 20)
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
     
     // MARK: - Navigation
@@ -28,12 +28,14 @@ class GalleryCollectionViewController: UICollectionViewController {
 
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 100
+        return photos.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "galleryCellIdentifier", for: indexPath) as! GalleryCollectionViewCell
-        cell.backgroundColor = .black
+        let imageString = photos[indexPath.item]
+        guard let image = UIImage(named: imageString) else { return cell }
+        cell.prepare(for: image)
         return cell
     }
 
