d6d8d1ad26a5331f1bdd79e3c784293ae1c84536
AlexZhadaev <pvt.vatnik@gmail.com>
2021-05-15 21:01:09 +0300

    estimatedItemSize fixed, photo added

--- GenericGalleryApp/GenericGalleryApp/ViewController/GalleryCollectionLayoutExtension.swift
+++ GenericGalleryApp/GenericGalleryApp/ViewController/GalleryCollectionLayoutExtension.swift
@@ -8,23 +8,24 @@
 import UIKit
 
 extension GalleryCollectionViewController: UICollectionViewDelegateFlowLayout {
-
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let spacingWidth = sectionInsets.left * (itemsPerRow + 1)
         let availableWidth = collectionView.frame.width - spacingWidth
-        let widthPerItem = availableWidth / itemsPerRow
+        let widthPerItem = round(availableWidth / itemsPerRow)
         return CGSize(width: widthPerItem, height: widthPerItem)
     }
-
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
         return sectionInsets
     }
-
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         return sectionInsets.left
     }
-
+    
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
         return sectionInsets.left
     }
+    
 }
